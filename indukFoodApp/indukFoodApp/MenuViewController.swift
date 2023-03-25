db787162da9b8baa47e5d9f0b576e85f29bd4af4
Jaeha Hwang <wogk216@naver.com>
2023-03-26 00:25:16 +0900

    뷰간 값 넘겨주기

--- indukFoodApp/indukFoodApp/MenuViewController.swift
+++ indukFoodApp/indukFoodApp/MenuViewController.swift
@@ -7,23 +7,44 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // 테이블, 전 테이블에서 가져온 식당이름과 식당이미지
+    @IBOutlet weak var mTable: UITableView! // 가게상세 테이블뷰
+    @IBOutlet weak var restImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+
+    // 지도, 전화버튼
+    @IBAction func mapBtn(_ sender: Any) {
+    }
+    @IBAction func callBtn(_ sender: Any) {
+    }
+    
+    
+    let restName2: [String] = ["카페니니", "하다식당", "민들레초밥", "한솥", "김가네", "노랑통닭"]
+    var hang: Int? // 전 뷰인 ViewController의 cell 선택시 보내지는 sender를 저장할 변수
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mTable.delegate = self
+        mTable.dataSource = self
+        
+        restImg?.image = UIImage(named: "\(hang ?? 0).png") // 행의 값(정수 0 ~ ...)을 받아와 그에 맞는 매장 이미지를 출력
+        nameLbl.text = restName2[hang ?? 0] // // 행의 값(정수 0 ~ ...)을 받아와 그에 맞는 매장 이름을 출력
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let mCell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath) as! MenuTableViewCell
+        mCell.menuLbl.text = "취킨"
+        mCell.priceLbl.text = "30000원"
+        return mCell
+    }
+    
+    
+    
 }
--- indukFoodApp/indukFoodApp/ViewController.swift
+++ indukFoodApp/indukFoodApp/ViewController.swift
@@ -9,34 +9,35 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let restName:[String] = ["카페니니", "하다식당", "민들레초밥", "한솥", "김가네", "노랑통닭"]
+    let restCategory:[String] = ["카페", "양식", "일식", "한식", "한식", "양식"]
+    
     @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // table의 dataSource과 delegate는 이 클래스에서 처리한다는 뜻 필수!!!
         table.dataSource = self
         table.delegate = self
-        
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1 // 섹션의 갯수를 2개로
+        return 1 // 섹션의 갯수를 n개로
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1 // row(행)의 갯수 10개로
+        return restName.count // row(행)의 갯수 n개로
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         // cell 직접 만들어 사용해보자 =========================================================================
         // cell을 MyTableViewCell로 다운캐스팅 하여 부모가 자식의 인스턴스를 쓸 수 있게함
         let cell = table.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
-        cell.nameLbl.text = "치킨"
-        cell.categoryLbl.text = "양식"
+        cell.nameLbl.text = self.restName[indexPath.row]
+        cell.categoryLbl.text = self.restCategory[indexPath.row]
         cell.resstImg.image = UIImage(named: "\(indexPath.row).png")
         return cell
-        
         
         // 제공 해주는 스타일의 cell 사용법! ========================================================================
         //        // UITableViewCell의 인스턴스 cell 생성
@@ -45,6 +46,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         //        cell.textLabel?.text = "\(indexPath.row)"
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+        print(indexPath.row)
+        print("눌렀네")
+    }
     
+    // MenuViewController로 정보를 이어줄 prepare함수
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segue.identifier값 확인
+        if segue.identifier == "showDetail" {
+            
+            // segue.destination이 MenuViewController가 맞다면 상수 vcSend를 MenuViewController로 다운캐스팅
+            let vcSend = segue.destination as? MenuViewController // 이제 vcSend로 MenuViewController의 프로퍼티에 접근가능함
+            
+            // sender 값을 firstVal에 대입
+            if let firstVal = sender as? Int? {
+                vcSend?.hang = firstVal //MenuViewController의 hang 변수에 firstVal 대입
+            }
+        }
+    }
 }
 
